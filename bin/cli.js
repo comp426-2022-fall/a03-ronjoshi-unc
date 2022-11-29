6d2c2422a58e991d6cf781f8bef03098739c8d21
ronjoshi-unc <ronjoshi@ad.unc.edu>
2022-11-29 16:26:10 -0500

    should work

--- bin/cli.js
+++ bin/cli.js
@@ -1,17 +1,4 @@
 #!/usr/bin/env node
-
-// import function created in other folder
-import Roll from "../lib/roll.js";
-// import minimist to process argv
-import minimist from "minimist";
-
-// get CLI argv and process them
-const args = minimist(process.argv.slice(2));
-
-// print output
-console.log(JSON.stringify(Roll(args.sides, args.dice, args.rolls)));
-
-/*#!/usr/bin/env node
 import Roll from "../lib/roll.js";
 import minimist from 'minimist';
 const args = minimist(process.argv.slice(2));
@@ -20,4 +7,4 @@ const sides = args.sides;
 const dice = args.dice;
 const rolls = args.rolls;
 
-console.log(JSON.stringify(Roll(sides, dice, rolls)));*/
+console.log(JSON.stringify(Roll(sides, dice, rolls)));
