2c620f7394e8e63dcc2e735da2f8dad92e9b6ef7
ronjoshi-unc <ronjoshi@ad.unc.edu>
2022-11-29 10:11:44 -0500

    cahnged syntax error

--- bin/cli.js
+++ bin/cli.js
@@ -7,4 +7,4 @@ const sides = args.sides || 6;
 const dice = args.dice || 2;
 const rolls = args.rolls || 1;
 
-console.log(JSON.stringify(roll(sides, dice, rolls)));
+console.log(JSON.stringify(Roll(sides, dice, rolls)));
